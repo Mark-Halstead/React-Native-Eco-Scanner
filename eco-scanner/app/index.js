b5b905e8f53f89d99950b8f6f363d12b0b355689
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2024-08-18 17:18:10 +0100

    fixed grey line

--- eco-scanner/app/index.js
+++ eco-scanner/app/index.js
@@ -11,7 +11,18 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#E8F5E9', // Set the background color of the header
+            borderBottomWidth: 0, // Remove the bottom border
+            elevation: 0, // Remove shadow on Android
+          },
+          headerTintColor: '#388E3C', // Set the color of the back button
+          headerTitle: '', // Remove the title from the header
+          headerShadowVisible: false, // Remove the shadow on iOS
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Scan" component={ScanScreen} />
         <Stack.Screen name="Result" component={ResultScreen} />
